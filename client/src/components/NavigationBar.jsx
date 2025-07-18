8a21d84737e0d652a8d2d05679793afe22dfad2d
pradnyabhakare <bhakarepradnya19@gmail.com>
2025-07-18 23:24:08 +0530

    fix front-end

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -3,7 +3,7 @@ import Wrapper from '../assets/wrappers/Navbar';
 import { FaAlignLeft } from 'react-icons/fa';
 import Logo from './Logo';
 import { useDashboardContext } from '../pages/DashboardLayout';
-import LogoutContainer from './logoutContainer';
+import LogoutContainer from './LogoutContainer';
 import Themetoggle from './Themetoggle';
 
 const NavigationBar = () => {
